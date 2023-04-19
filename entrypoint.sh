875e3370fe33bea215f8570b295868e71e980244
Michal Czyz <mczyz@antmicro.com>
2023-04-19 14:10:53 +0200

    Migrate to verible-indexer-action

---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+#━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+# Start the http server
+
+echo "Starting the http server"
+
+/home/indexer/indexer/run-kythe-server.sh
+
+exit 0
